0839c35ad8319392c19ac6456ef813248f116aad
sharda maurya <shardamaurya42570@gmail.com>
2023-08-15 23:18:49 +0530

    string

---
+++ 5-string-program3.py
@@ -0,0 +1,9 @@
+# Create two variables to store how many fruits and vegetables you eat in a 
+# day. Now Print "I eat x veggies and y fruits daily" where x and y presents 
+# vegetables and fruits that you eat everyday. Use python f string for this.
+
+vegetables=5
+fruits=4
+
+string=f'I eat {vegetables} veggies and {fruits} fruits daily'
+print(string)
