ec69d56ac5cbe04201608607fcadb7c683479f75
sharda maurya <shardamaurya42570@gmail.com>
2023-08-15 23:18:04 +0530

    string

---
+++ 4-string-program2.py
@@ -0,0 +1,9 @@
+# Create a variable to store the string "Earth revolves around the sun"
+#  i. Print "revolves" using slice operator
+# ii. Print "sun" using negative index
+
+
+s1='earth revolves around the sun'
+print("print revolves using slice operator = ",s1[6:15])
+
+print("print sun using negative index = ",s1[-3:])
