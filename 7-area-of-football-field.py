eeb7c7ab78a390fdcc00d74c2cbbe4d932beec07
sharda maurya <shardamaurya42570@gmail.com>
2023-08-23 23:35:52 +0530

    numbers

---
+++ 7-area-of-football-field.py
@@ -0,0 +1,7 @@
+# You have a football field that is 92 meter long and 48.8 meter wide.
+# Find out total area using python and print it.
+
+length=92
+width=48.8
+area=length*width
+print("area of football field = ",area,"meter square")
