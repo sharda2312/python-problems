6b75bad3a060dfdc47c510e4b8b55638e3ad6419
sharda maurya <shardamaurya42570@gmail.com>
2023-08-23 23:37:27 +0530

    numbers

---
+++ 9-shopkeeper-potato-chips.py
@@ -0,0 +1,8 @@
+# queston: You bought 9 packets of potato chips from a store. Each packet costs 
+# 1.49 dollar and you gave shopkeeper 20 dollar. Find out using python,
+# how many dollars is the shopkeeper going to give you back?
+totalchips=9
+cost=1.49
+amount=totalchips*cost
+paidamount=20
+print("amount shopkeeper will return is",paidamount-amount)
