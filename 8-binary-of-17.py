557edcc5784bf8d432ad962d40719d3150335076
sharda maurya <shardamaurya42570@gmail.com>
2023-08-23 23:36:20 +0530

    numbers

---
+++ 8-binary-of-17.py
@@ -0,0 +1,8 @@
+# queation: Print binary representation of number 17
+number = 17
+binary_representation = bin(number)
+print(binary_representation)
+
+# 4. Print binary representation of number 17
+num=17
+print('Binary of number 17 is:',format(num,'b')) 
