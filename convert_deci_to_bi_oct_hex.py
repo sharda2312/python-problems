c0b5a2edd6738eabf2a558fb78adea627f75b011
sharda maurya <shardamaurya42570@gmail.com>
2023-09-01 23:52:03 +0530

    functions

---
+++ convert_deci_to_bi_oct_hex.py
@@ -0,0 +1,7 @@
+# Program to convert decimal to binary, octal and hexdecimal.
+
+decimal = input("Please enter decimal:")
+decimal=int(decimal)
+print(bin(decimal),"in binary.")
+print(oct(decimal),"in octal.")
+print(hex(decimal),"in hexadecimal.")
