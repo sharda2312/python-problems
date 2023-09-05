46ddd633b4a91f124c56dc43b46a9a8c9fe6f5d6
sharda maurya <shardamaurya42570@gmail.com>
2023-09-05 21:58:32 +0530

    if else

---
+++ positvenegativezero.py
@@ -0,0 +1,12 @@
+# program to if number entered is positive or negative.
+
+num = input("enter the number:")
+num = int(num)
+if num > 0:
+    print("the number is positive")
+
+elif num < 0:
+    print("the number is negative")
+
+else:
+    print("the number is zero")
