e3eef572c81e88b23abe2d8d7aaba6857819285e
sharda maurya <shardamaurya42570@gmail.com>
2023-09-05 22:00:22 +0530

    if else

---
+++ sumofnaturalnumber.py
@@ -0,0 +1,12 @@
+num = input("enter the number:")
+num = int(num)
+# sum=0
+# for j in range(0, num+1):
+#     sum=sum+j
+    
+# print("the sum natural number upto", num,"is",sum )
+
+sum=(num*(num+1))//2
+print("the sum natural number upto", num,"is",sum )
+
+    
